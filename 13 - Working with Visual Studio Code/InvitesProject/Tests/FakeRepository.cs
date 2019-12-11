43f5f10e7e770f2e0b908992a8349beb6dc687e0
Camiel Luijkx <cluijkx@netaspect.nl>
2019-12-11 18:25:20 +0100

    added missing file

---
+++ 13 - Working with Visual Studio Code/InvitesProject/Tests/FakeRepository.cs
@@ -0,0 +1,22 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    internal class FakeRepository : IRepository
+    {
+        public IEnumerable<GuestResponse> Responses { get; } 
+            = new List<GuestResponse>
+            {
+                new GuestResponse { Name = "Bob", WillAttend = true },
+                new GuestResponse { Name = "Alice", WillAttend = true },
+                new GuestResponse { Name = "Joe", WillAttend = false }
+            };
+
+        public void AddResponse(GuestResponse response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
