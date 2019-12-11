3c75d5ac47e579bfe562cb0a6c1a0bcddcaf3055
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-12-11 13:52:46 +0100

    added missing files

---
+++ 12 - SportsStore - Security & Deployment/SportsStore/SportsStore/Migrations/ApplicationDb/20170816162555_ShippedOrders.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations.ApplicationDb
+{
+    /// <summary>
+    /// Database migration created with command: dotnet ef migrations add ShippedOrders
+    /// </summary>
+    /// <remarks>
+    /// Database can be removed with command: dotnet ef database drop --force
+    /// Database can be updated with command: dotnet ef database update --context ApplicationDbContext
+    /// </remarks>
+    public partial class ShippedOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+        }
+    }
+}
---
+++ 12 - SportsStore - Security & Deployment/SportsStore/SportsStore/Models/ApplicationDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    // [TODO] purpose of this database context factory?
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            return Program.BuildWebHost(args).Services
+                .GetRequiredService<ApplicationDbContext>();
+        }
+    }
+}
